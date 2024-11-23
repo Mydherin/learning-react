41aff17ff22573b8e2fca27f0de46cd2f321d916
Mydherin <mydherin@gmail.com>
2024-11-23 12:55:09 +0100

    feat (3_mui-text-field): add text field component

---
+++ 3_mui-text-field/src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./App.css";
+
+const App = () => {
+  return (
+    <main>
+      <div className="container">
+        <label>
+          <input type="text" placeholder="Name" />
+          <span>Name</span>
+        </label>
+      </div>
+    </main>
+  )
+}
+
+export default App
